d005e82701c28e48049b107f51008a7819c1e74e
miguelta281@gmail.com <miguelta281@gmail.com>
2021-03-04 01:40:09 -0500

    Actualice el calculo de c_ambient

--- Experimentos/Dispositivo/ach.py
+++ Experimentos/Dispositivo/ach.py
@@ -26,17 +26,22 @@ def calidad (ACH):
     return calidad
 
 
-def ACH (name:str,start_date:str,end_date:str):
+def ACH (name:str,time:list):
     """
     Parameters:
     name:str -> Ruta del archivo que se quiere cargar
     """
+    start_date = time[2]
+    end_date = time[3]
     data = pd.read_csv(name)
     data.index = pd.DatetimeIndex(data.timestamp)
     data = data.resample('1T').mean()
+
+    data_1 = cutdata([data],time[0],time[1])[0]
+    data_2 = cutdata([data],time[4],time[5])[0]
     data = cutdata([data],start_date,end_date)[0]
 
-    c_ambient = 1000
+    c_ambient = np.mean(np.array([float(data_2.CO2.mean()), float(data_1.CO2.mean())]))
 
     aux = int(list(np.where(data['CO2'] == float(data.CO2.max())))[0])
     c_star =float(data.CO2.max())
@@ -44,7 +49,7 @@ def ACH (name:str,start_date:str,end_date:str):
     c_end = c_ambient +  0.37 * (int(data.max() - c_ambient))
     t_end = data[aux:][data['CO2'][aux:] <= c_end].index.tolist()[0]
 
-    ACH = (-1 * np.log((c_end-c_ambient)/(c_star - c_ambient)))/((t_end -t_star).seconds / 3600)
+    ACH =  (-1 * np.log((c_end-c_ambient)/(c_star - c_ambient)))/((t_end -t_star).seconds / 3600)
 
 
     plt.figure(figsize=(18,6))
@@ -59,4 +64,4 @@ def ACH (name:str,start_date:str,end_date:str):
     plt.show()
 
     print( 'Calidad:', calidad(ACH), ', ACH:', ACH)
-    return (calidad,ACH)
+    return (calidad(ACH),ACH,c_ambient)
